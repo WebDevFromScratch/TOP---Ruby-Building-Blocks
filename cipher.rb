4bd90b853b4f7e42ef399f6216eadcfb52402d9a
Piotr Klosinski <mybettingstuff@gmail.com>
2014-06-23 12:19:30 +0200

    polished the code a little

--- cipher.rb
+++ cipher.rb
@@ -11,32 +11,34 @@
 @letters = ('a'..'z').to_a #size = 26
 @capital_letters = ('A'..'Z').to_a
 
-puts "Write a message to decode!"
-string = gets.chomp
+def caesar_cipher
+  puts "Write a message to decode!"
+  string = gets.chomp
+  puts "What should the shift be?"
+  shift = gets.chomp.to_i
 
-puts ""
+  string = string.split(//) #turned into an array
 
-# changing the index of the letters from the input string
+  string.each do |char|
+    if !@letters.include?(char) && !@capital_letters.include?(char)
+      print char
+    elsif @capital_letters.include?(char)
+      index = @capital_letters.index(char)
+      index += shift
+      index %= @capital_letters.size
 
-string = string.split(//) #turned into an array
+      print @capital_letters[index]
+    else
+      index = @letters.index(char)
+      index += shift
+      index %= @letters.size
 
-string.each do |char|
-  if !@letters.include?(char) && !@capital_letters.include?(char)
-    print char
-  elsif @capital_letters.include?(char)
-    index = @capital_letters.index(char)
-    index += 3
-    index %= 26
-
-    print @capital_letters[index]
-  else
-    index = @letters.index(char)
-    index += 3
-    index %= 26
-
-    print @letters[index]
+      print @letters[index]
+    end
   end
 end
+
+caesar_cipher
 
 puts ""
 
